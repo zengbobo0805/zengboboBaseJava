641f2e2db511f6787b93bbe16b68cdb4bef0247e
zengbobo0805 <boy812796958@aliyun.com>
2015-09-01 18:31:35 +0800

    2016/05/05 17:42

--- src/com/zengboboBaseJava/TestFirst.java
+++ src/com/zengboboBaseJava/TestFirst.java
@@ -1,5 +1,7 @@
 package com.zengboboBaseJava;
 
+import java.lang.System;
+
 public class TestFirst {
 
 	/**
@@ -7,7 +9,14 @@ public class TestFirst {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+//
+//		 AtomicBoolean isCancelled = new AtomicBoolean();
+//		System.out.println("TestFirst isCancelled:"+isCancelled.get());
 
+		int i=5,j=5,p,q;
+		p = (i++)+(i++)+(i++);
+		q = (++j)+(++j)+(++j);
+		System.out.println("TestFirst p:"+p+",q:"+q+",i:"+i+",j:"+j);
 	}
 
 }
